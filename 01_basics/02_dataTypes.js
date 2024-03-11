18cd797b410af3e52ff38a948098f804569973e7
Vivekumar22 <111337482+Vivekumar22@users.noreply.github.com>
2024-03-11 05:54:16 +0000

    container config done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3 + 3). // we are using nodejs ; not browser
+
+let age18
+let isLogin = false
+let state;
+
+
+/* premitive datatypes
+
+ number => 2 to poewer 53
+ bigint
+ string => " "
+ boolean =>true/false
+ null => standard vlue means ek dum khali kuch v nhi hai you can write let s=null;
+ null is a object
+ undefines => 
+ symbol => unique
+
+*/
+
+//object
+console.log(typeof undefined);
+console.log(typeof null);
