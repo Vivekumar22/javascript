5b63866bb09070123c636d6786d3c566dce8ff2c
Vivekumar22 <111337482+Vivekumar22@users.noreply.github.com>
2024-03-13 03:14:01 +0000

    container config done

--- 02_basics/02_Arrays.js
+++ 02_basics/02_Arrays.js
@@ -1,21 +1,21 @@
 const marvel_heros = ["thor","ironma","spiderman"]
 const dc_heros = ["superman","Batman","flash"]
 
-//marvel_heros.push(dc_heros)
+marvel_heros.push(dc_heros)
 
-// console.log(marvel_heros);
-// console.log(marvel_heros[3][1]);
+console.log(marvel_heros);
+console.log(marvel_heros[3][1]);
 
-// const allheroes = marvel_heros.concat(dc_heros) //concat make new array
-// console.log(allheroes);
+const allheroes = marvel_heros.concat(dc_heros) //concat make new array
+console.log(allheroes);
 
 const all_new_heros = [...marvel_heros, ...dc_heros] //spread do same thing as concat
-//console.log(all_new_heros);
+console.log(all_new_heros);
 
 const another_array  = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
 
 const real_another_array = another_array.flat(Infinity) //flat make one array
-//console.log(real_another_array);
+console.log(real_another_array);
 
 console.log(Array.isArray("vivek"));
 console.log(Array.from("vivek")); // change to array
