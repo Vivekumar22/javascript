f5a415e71ecc7756a6d9fb1568bda26563996312
Vivekumar22 <111337482+Vivekumar22@users.noreply.github.com>
2024-03-18 04:49:41 +0000

    vivek

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -10,7 +10,7 @@
 
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-// const newNums = myNums.filter( (num) => {
+// const newNums = myNums.filter( (num) => {  filter return but foreach don't
 //     return num > 4
 // } )
 
