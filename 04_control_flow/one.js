e4433b5390020b5a6e61551067370cfa274c7f8f
Vivekumar22 <111337482+Vivekumar22@users.noreply.github.com>
2024-03-21 11:17:11 +0530

    Update one.js

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,51 +1,51 @@
-// // if
+// if
 
-// const isUserLoggedIn = true
-// const temperature = 410
+const isUserLoggedIn = true
+const temperature = 410
 
-// if(temperature === 41){
-//     console.log("less than 50");
-// } else{
-//     console.log("temperature is greater than 50");
-// }
+if(temperature === 41){
+    console.log("less than 50");
+} else{
+    console.log("temperature is greater than 50");
+}
 
-// // <, >, <=, >=, ==, !=, ===, !==
+// <, >, <=, >=, ==, !=, ===, !==
 
-// const score = 200
+const score = 200
 
-// if(score > 100){
-//     const power = "fly"
-//     console.log(`user power: ${power}`);
-// }
+if(score > 100){
+    const power = "fly"
+    console.log(`user power: ${power}`);
+}
 
-//const balence = 1000
+const balence = 1000
 
-// if(balence> 500) console.log("test"),
-// console.log("test2");
+if(balence> 500) console.log("test"),
+console.log("test2");
 
 
-// if(balence < 500){
-//     console.log("less than 500 ");
-// } else if (balence <750) {
-//     console.log("less than 750");
-// }else if (balence <900) {
-//     console.log("less than 900");
-// }else {
-//     console.log("less than 1200");
-// }
+if(balence < 500){
+    console.log("less than 500 ");
+} else if (balence <750) {
+    console.log("less than 750");
+}else if (balence <900) {
+    console.log("less than 900");
+}else {
+    console.log("less than 1200");
+}
 
-// const userLoggedIn = true
-// const debitCard = true
-// const loggedInFromGoogle = false
-// const loggedInFromEmail = true
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
 
-// if(userLoggedIn && debitCard && 2==2){
-//     console.log("Allow to buy course");
-// }
+if(userLoggedIn && debitCard && 2==2){
+    console.log("Allow to buy course");
+}
 
-// if(loggedInFromGoogle || loggedInFromEmail){
-//     console.log("User logged in");
-// }
+if(loggedInFromGoogle || loggedInFromEmail){
+    console.log("User logged in");
+}
 
 
 
